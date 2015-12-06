29ae3cd49d5423562c37f8f8510379bfbf13cb39
jianganglu <jianganglu@hengtiansoft.com>
2015-12-06 10:06:32 +0800

    i18n

---
+++ www/js/filters/filters.js
@@ -0,0 +1,18 @@
+﻿angular.module('starter')
+.filter('T', function T($translate) {
+    return function(key) {
+        if(key) {
+            return $translate.instant(key);
+        }
+        return key;
+    }
+})
+.filter('map', function() {
+    var filter = function(input) {
+        return input + '...';
+    };
+    return filter;
+});
+
+
+
