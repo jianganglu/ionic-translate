820825fab152229543f5c1b1df28d1d21fd5467f
jianganglu <jianganglu@hengtiansoft.com>
2015-12-11 19:41:41 +0800

    update

--- www/js/configs/common.js
+++ www/js/configs/common.js
@@ -1,6 +1,6 @@
 ﻿(function() {
   angular.module('starter')
-    .run(function($ionicPlatform) {
+    .run(['$ionicPlatform', function($ionicPlatform) {
       $ionicPlatform.ready(function() {
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
@@ -9,10 +9,19 @@
           cordova.plugins.Keyboard.disableScroll(true);
 
         }
+
+        if(window.plugins && window.plugins.jPushPlugin) {
+          window.plugins.jPushPlugin.init();
+        }
+
+        console.log(alert(JSON.stringify(window.cordova)));
+        console.log(alert(JSON.stringify(window.cordova.plugins)));
+        console.log(alert(window.cordova.plugins.Keyboard));
+
         if (window.StatusBar) {
           // org.apache.cordova.statusbar required
           StatusBar.styleDefault();
         }
       });
-    });
+    }]);
 })();
